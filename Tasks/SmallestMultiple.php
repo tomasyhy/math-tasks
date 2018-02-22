3f62b3382838388cdb6b58b0c0c0a25367a7c89d
Tomasyhy <tomasz.hliwiadczyn@gmail.com>
2018-02-22 22:27:33 +0100

    SmallestMultiple

---
+++ Tasks/SmallestMultiple.php
@@ -0,0 +1,24 @@
+<?php
+require_once 'TaskInterface.php';
+
+
+class SmallestMultiple implements TaskInterface
+{
+
+    public function run(): int
+    {
+        $max = 20;
+        $dividers = range(1, $max);
+
+        $i = 1;
+        while (true) {
+            foreach ($dividers as $divider) {
+                if ($i % $divider !== 0) {
+                    $i++;
+                    continue 2;
+                }
+            }
+            return $i;
+        }
+    }
+}
